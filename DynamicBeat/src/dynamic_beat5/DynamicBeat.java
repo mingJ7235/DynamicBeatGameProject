d313247e4ec647ae5a3bfb3d2a4f32cf8581615d
김민재 <joshuara7235@gmail.com>
2021-04-24 16:19:51 +0900

    button for start and quit

---
+++ DynamicBeat/src/dynamic_beat5/DynamicBeat.java
@@ -0,0 +1,171 @@
+package dynamic_beat5;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class DynamicBeat extends JFrame {
+
+	private Image screenImage;
+	private Graphics screenGraphic;
+
+	private Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground.jpg")).getImage();
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+	private ImageIcon exitButtonEnterdImage = new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
+	private ImageIcon startButtonImage = new ImageIcon (Main.class.getResource("../images/startButtonBasic.png"));
+	private ImageIcon quitButtonEnterdImage = new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
+	
+	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
+	
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+	private JButton startButton = new JButton(startButtonImage);
+	private JButton quitButton = new JButton(quitButtonEnterdImage);
+	
+	private int mouseX, mouseY;
+	
+	public DynamicBeat() {
+		//setUndecorated(true);
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		// 프로그램이 정중앙에 오도록 함
+		setLocationRelativeTo(null);
+		// 창을 끄면 프로그램이 종료되도록 함
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		//setBackground(new Color(0, 0, 0, 0));
+		//setLayout(null);
+		
+		exitButton.setBounds(1245, 0, 30, 30);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(exitButtonEnterdImage);
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music ("", false); //버튼 눌렀을때 노래넣기 (src)
+				buttonEnteredMusic.start();
+			}
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(exitButtonBasicImage);
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music ("", false);
+				buttonEnteredMusic.start();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
+		add(exitButton);
+		
+		//startbtn
+		startButton.setBounds(40, 200, 400, 100);
+		startButton.setBorderPainted(false);
+		startButton.setContentAreaFilled(false);
+		startButton.setFocusPainted(false);
+		startButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				startButton.setIcon(startButtonImage);
+				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music ("", false); //버튼 눌렀을때 노래넣기 (src)
+				buttonEnteredMusic.start();
+			}
+			public void mouseExited(MouseEvent e) {
+				startButton.setIcon(startButtonImage);
+				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music ("", false);
+				buttonEnteredMusic.start();
+				//게임시작 이벤트
+			}
+		});
+		add(startButton);
+		
+		//quitbtn
+		quitButton.setBounds(40, 330, 400, 100);
+		quitButton.setBorderPainted(false);
+		quitButton.setContentAreaFilled(false);
+		quitButton.setFocusPainted(false);
+		quitButton.addMouseListener(new MouseAdapter() {
+			public void mouseEntered(MouseEvent e) {
+				quitButton.setIcon(quitButtonEnterdImage);
+				quitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music ("", false); //버튼 눌렀을때 노래넣기 (src)
+				buttonEnteredMusic.start();
+			}
+			public void mouseExited(MouseEvent e) {
+				quitButton.setIcon(quitButtonEnterdImage);
+				quitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnteredMusic = new Music ("", false);
+				buttonEnteredMusic.start();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
+		add(quitButton);
+
+		menuBar.setBounds(0, 0, 1280, 30);
+		menuBar.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				mouseX =e.getX();
+				mouseY =e.getY();
+			}
+		});
+		
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);
+			}
+		});
+		add(menuBar);
+
+		
+		
+		Music introMusic = new Music("introMusic.mp3", true);
+		introMusic.start();
+
+	}
+
+	// 더블 버퍼링 방법 -> 버퍼에 이미지를 담아서 매순간 이미지를 로딩함
+
+	// paint라는 약속된 메소드가 있다. 프로그램 화면크기만큼 이미지를 받아와서 그려준다 - > screenDraw로 그려준다.
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+
+	// repaint로 계속 이 프로그램이 돌아가면서 화면에 지속적으로 그려준다.
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		//paintComponents(g);
+		this.repaint();
+	}
+
+}
